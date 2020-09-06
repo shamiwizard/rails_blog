9dc51da74c15631ee06689137f7ff82da06a135b
shamiwizard <leibyuk04mi@gmail.com>
2020-09-07 00:21:14 +0300

    add new field to users, change sign_up view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :last_name, presence: true, length: { maximum: 250 }
+  validates :first_name, presence: true, length: { maximum: 250 }
+  validates :username, presence: true, length: { maximum: 250 }
+  validates :password, confirmation: true, length: { in: 6..30 }
+
 end
